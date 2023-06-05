282b2b5e397647456624d1f6943ba9978f890017
Fabrizio Ciacchi <fabrizio.ciacchi@gmail.com>
2023-06-05 15:47:35 +0200

    Newever, refined version of the script

--- contentful-cli-migrations.js
+++ contentful-cli-migrations.js
@@ -107,23 +107,19 @@ async function parseArguments(rootFolder, envValues) {
   const parsedArgs = minimist(process.argv.slice(2))
   await checkArgs(parsedArgs)
 
-  let environmentId = null
-  const spaceId =
-    parsedArgs['space-id'] ?? envValues?.CMS_SPACE_ID ?? PLACEHOLDER_SPACE_ID
-  const managementToken =
-    parsedArgs['management-token'] ??
-    parsedArgs['mt'] ??
-    envValues?.CMS_MANAGEMENT_TOKEN ??
-    PLACEHOLDER_MANAGEMENT_TOKEN
-  const counterEntryId =
-    parsedArgs['counter-id'] ?? envValues?.CMS_MIGRATIONS_COUNTER_ID
-  const counterFieldId =
-    parsedArgs['counter-field'] ?? envValues?.CMS_MIGRATIONS_COUNTER_FIELD
-  const counterLocale =
-    parsedArgs['counter-locale'] ??
-    envValues?.CMS_MIGRATIONS_COUNTER_LOCALE ??
-    DEFAULT_LOCALE
-  const migrationsDir = envValues?.CMS_MIGRATIONS_DIR ?? DEFAULT_MIGRATIONS_DIR
+  const {
+    'space-id': spaceId = envValues?.CMS_SPACE_ID ?? PLACEHOLDER_SPACE_ID,
+    'management-token': managementToken = parsedArgs['mt'] ??
+      envValues?.CMS_MANAGEMENT_TOKEN ??
+      PLACEHOLDER_MANAGEMENT_TOKEN,
+    'counter-id': counterEntryId = envValues?.CMS_MIGRATIONS_COUNTER_ID,
+    'counter-field': counterFieldId = envValues?.CMS_MIGRATIONS_COUNTER_FIELD,
+    'counter-locale':
+      counterLocale = envValues?.CMS_MIGRATIONS_COUNTER_LOCALE ??
+        DEFAULT_LOCALE,
+    'migrations-dir': migrationsDir = envValues?.CMS_MIGRATIONS_DIR ??
+      DEFAULT_MIGRATIONS_DIR
+  } = parsedArgs
 
   const rootDestinationFolder = await getDestinationFolder(
     rootFolder,
@@ -131,12 +127,8 @@ async function parseArguments(rootFolder, envValues) {
     parsedArgs
   )
 
-  if (
-    parsedArgs.hasOwnProperty('to') ||
-    parsedArgs.hasOwnProperty('environment-id')
-  ) {
-    environmentId = parsedArgs.to ?? parsedArgs['environment-id']
-  } else {
+  const environmentId = parsedArgs.to || parsedArgs['environment-id']
+  if (!environmentId) {
     console.error('@@/ERROR: An environment-id should be specified')
     process.exit(1)
   }
@@ -157,30 +149,28 @@ async function parseArguments(rootFolder, envValues) {
  * This function checks the arguments passed in the command line.
  *
  * @param {Object} parsedArgs - The object that contains the parsed command line arguments.
+ * @property {string} managementToken - The CMS Management Token.
+ * @property {string} spaceId - The CMS Space ID.
+ * @property {string} environmentId - The CMS Environment ID.
+ * @property {string} rootDestinationFolder - The folder containing the migrations
+ * @property {string} counterEntryId - The entry ID storing the counter.
+ * @property {string} counterFieldId - The field ID to retrieve the latest ran migration.
+ * @property {string} counterLocale - The locale of the field to look for.
+ * @property {boolean} forceYes - If should run all the migrations.
  * @returns {Promise<object>} An object containing the evaluated command line arguments.
  *
  * @throws {Error} If both 'to' and 'environment-id' options are specified or if neither is specified.
  * @throws {Error} If both 'management-token' and 'mt' options are specified.
  */
 async function checkArgs(parsedArgs) {
-  if (
-    (parsedArgs.hasOwnProperty('to') &&
-      parsedArgs.hasOwnProperty('environment-id')) ||
-    !(
-      parsedArgs.hasOwnProperty('to') ||
-      parsedArgs.hasOwnProperty('environment-id')
-    )
-  ) {
+  if (!(Boolean(parsedArgs.to) ^ Boolean(parsedArgs['environment-id']))) {
     console.error(
       "@@/ERROR: Only one of the two options '--environment-id' or '--to' should be specified"
     )
     process.exit(1)
   }
 
-  if (
-    parsedArgs.hasOwnProperty('management-token') &&
-    parsedArgs.hasOwnProperty('mt')
-  ) {
+  if (Boolean(parsedArgs['management-token']) && Boolean(parsedArgs.mt)) {
     console.error(
       "@@/ERROR: Only one of the two options '--management-token' or '--mt' can be specified"
     )
@@ -194,6 +184,14 @@ async function checkArgs(parsedArgs) {
  * @param {string} rootFolder - The directory path where the script is being executed.
  * @param {string} cmsMigrationsDir - The CMS Default Migrations Directory.
  * @param {Object} parsedArgs - The object that contains the parsed command line arguments.
+ * @property {string} managementToken - The CMS Management Token.
+ * @property {string} spaceId - The CMS Space ID.
+ * @property {string} environmentId - The CMS Environment ID.
+ * @property {string} rootDestinationFolder - The folder containing the migrations
+ * @property {string} counterEntryId - The entry ID storing the counter.
+ * @property {string} counterFieldId - The field ID to retrieve the latest ran migration.
+ * @property {string} counterLocale - The locale of the field to look for.
+ * @property {boolean} forceYes - If should run all the migrations.
  *
  * @returns {Promise<string>} The path of the evaluated destination folder.
  * @property {string} destinationFolder - The destination folder for the export.
@@ -202,21 +200,22 @@ async function checkArgs(parsedArgs) {
  */
 async function getDestinationFolder(rootFolder, cmsMigrationsDir, parsedArgs) {
   const fileSystem = await import('fs')
+  const path = await import('path')
 
-  const defaultExportDirectory = cmsMigrationsDir.startsWith('/')
-    ? cmsMigrationsDir
-    : `${rootFolder}/${cmsMigrationsDir}`
+  const defaultExportDirectory = path.join(rootFolder, cmsMigrationsDir)
 
-  let destinationFolder = parsedArgs['migrations-dir'] || defaultExportDirectory
-  destinationFolder = destinationFolder.replace(/\/$/, '') + '/'
+  let destinationFolder =
+    path.resolve(parsedArgs['migrations-dir'] || defaultExportDirectory) + '/'
 
   // Create destination folder if not present
-  const destinationFolderExists = fileSystem.existsSync(destinationFolder)
-  if (!parsedArgs['migrations-dir'] && !destinationFolderExists) {
+  if (!fileSystem.existsSync(destinationFolder)) {
     fileSystem.mkdirSync(destinationFolder, { recursive: true })
   }
 
-  if (!fileSystem.existsSync(destinationFolder) || destinationFolder === '/') {
+  if (
+    !fileSystem.existsSync(destinationFolder) ||
+    destinationFolder === path.sep
+  ) {
     console.error(
       '@@/ERROR: Destination folder does not exist or is not accessible!'
     )
@@ -262,14 +261,10 @@ async function getEnvironment(parsedArguments) {
 
   if (!environmentSingleton) {
     console.error(
-      "@@/ERROR: Unable to retrieve Destination environment-id '" +
-        parsedArguments?.environmentId +
-        "' for space-id '" +
-        parsedArguments?.spaceId +
-        "'!"
+      `Unable to retrieve Destination environment-id '${parsedArguments?.environmentId}' for space-id '${parsedArguments?.spaceId}'!`
     )
     console.error(
-      '@@/ERROR: Could also be that the management token or space-id are invalid.'
+      `Could also be that the management token or space-id are invalid.`
     )
     process.exit(1)
   }
@@ -282,6 +277,14 @@ async function getEnvironment(parsedArguments) {
  *
  * @param {import("contentful-management/dist/typings/entities/environment").Environment} environmentSingleton - The Contentful environment object.
  * @param {Object} parsedArguments - The script arguments, containing the counter entry-id information
+ * @property {string} managementToken - The CMS Management Token.
+ * @property {string} spaceId - The CMS Space ID.
+ * @property {string} environmentId - The CMS Environment ID.
+ * @property {string} rootDestinationFolder - The folder containing the migrations
+ * @property {string} counterEntryId - The entry ID storing the counter.
+ * @property {string} counterFieldId - The field ID to retrieve the latest ran migration.
+ * @property {string} counterLocale - The locale of the field to look for.
+ * @property {boolean} forceYes - If should run all the migrations.
  * @property {string} counterEntryId - The entry ID storing the counter.
  * @property {string} counterFieldId - The field ID to retrieve the latest ran migration.
  * @property {string} counterLocale - The locale of the field to look for.
@@ -295,18 +298,33 @@ async function getCounter(environmentSingleton, parsedArguments) {
       parsedArguments?.counterEntryId
     )
 
-    const counterIndex = counterEntry?.fields?.[fieldId]?.[fieldLocale] ?? false
-    return counterIndex ? parseInt(counterIndex, 10) : 0
+    if (!counterEntry?.fields?.[fieldId]) {
+      throw new Error('Impossible to retrieve the Counter entry!')
+    }
+
+    return parseInt(counterEntry.fields[fieldId]?.[fieldLocale], 10) || 0
   } catch (error) {
     console.error('@@/ERROR:', error)
+    process.exit(1)
   }
-
-  console.error('@@/ERROR: Impossible to retrieve the Counter entry!')
-  process.exit(1)
 }
 
+/**
+ * Parses the migrations to run based on the latest migration number.
+ *
+ * @param {Object} parsedArguments - The script arguments.
+ * @property {string} managementToken - The CMS Management Token.
+ * @property {string} spaceId - The CMS Space ID.
+ * @property {string} environmentId - The CMS Environment ID.
+ * @property {string} rootDestinationFolder - The folder containing the migrations
+ * @property {string} counterEntryId - The entry ID storing the counter.
+ * @property {string} counterFieldId - The field ID to retrieve the latest ran migration.
+ * @property {string} counterLocale - The locale of the field to look for.
+ * @property {boolean} forceYes - If should run all the migrations.
+ * @param {number} latestMigrationNumber - The latest migration number.
+ * @returns {Promise<string[]>} An array of migrations to run.
+ */
 async function parseMigrationsToRun(parsedArguments, latestMigrationNumber) {
-  // Compute the array of files to run migration
   const fileSystem = await import('fs')
   const folderMigrationScript = parsedArguments?.rootDestinationFolder
 
@@ -343,6 +361,15 @@ async function parseMigrationsToRun(parsedArguments, latestMigrationNumber) {
   return migrationArray
 }
 
+/**
+ * Performs the Contentful migrations.
+ *
+ * @param {import("contentful-management/dist/typings/entities/environment").Environment} environmentSingleton - The Contentful environment object.
+ * @param {Object} parsedArguments - The script arguments.
+ * @param {number} latestMigrationNumber - The latest migration number.
+ * @param {string[]} migrationArray - An array of migrations to run.
+ * @returns {Promise<void>}
+ */
 async function performMigrations(
   environmentSingleton,
   parsedArguments,
